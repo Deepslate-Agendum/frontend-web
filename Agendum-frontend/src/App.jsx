88f95ab0cfdc4255b251ce75362cc5f1949db6da
djsv3b <130416210+djsv3b@users.noreply.github.com>
2025-02-04 10:28:56 -0600

    Mongo setup (needs db info)

--- Agendum-frontend/src/App.jsx
+++ Agendum-frontend/src/App.jsx
@@ -1,6 +1,20 @@
 import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+
+    fetch("http://localhost:5000/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+  
   return (
     <>
     <div>
@@ -8,9 +22,11 @@ function App() {
       <div className='row'>
         <div>
           <h2>Fetch Users</h2>
+          <ul>{users.map((user) => <li key={user._id}>{user.name}</li>)}</ul>
         </div>
         <div>
           <h2>Fetch Tasks of Users</h2>
+          <ul>{tasks.map((task) => <li key={task._id}>{task.title}</li>)}</ul>
         </div>
 
       </div>
